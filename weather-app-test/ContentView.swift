b32521293a14c9cd69ca35580926320bf41fe6cc
Dmitry Volkov <dmitrijvolkov@MacBook-Air-Dmitry.local>
2025-05-28 14:25:49 -0500

    Initial commit

---
+++ weather-app-test/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  weather-app-test
+//
+//  Created by Dmitry Volkov on 28/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ weather-app-test/weather_app_testApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_app_testApp.swift
+//  weather-app-test
+//
+//  Created by Dmitry Volkov on 28/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct weather_app_testApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
